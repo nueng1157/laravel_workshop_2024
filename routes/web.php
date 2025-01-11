c8ea55ed1f6bee4381b581dcc99ac1e8780714cb
nueng1157 <nueng1157@gmail.com>
2025-01-11 22:14:38 +0700

    day 14

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\RoomController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/dashboard', [DashboardController::class, 'dashboard']);
 Route::get('/company/index', [CompanyController::class, 'index']);
+Route::get('/room', [RoomController::class, 'index']);
 
